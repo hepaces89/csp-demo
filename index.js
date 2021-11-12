a8c85bf1a70d4d99c4afa0e8b63484cf4715a3c0
Hubert Pan <hubert.pan3@gmail.com>
2021-11-12 00:07:50 -0500

    checkpoint

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const fs = require('fs')
 var Mustache = require('mustache');
+var crypto = require("crypto");
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.set("Content-Security-Policy-Report-Only", "default-src 'self'; report-uri /csp-report")
     fs.readFile('./templates/template.html', 'utf8', (err, content) => {
-        res.send(Mustache.render(content, {nonce: 'bob'}))
+        let nonceVal = crypto.randomBytes(20).toString('hex');
+        res.set("Content-Security-Policy-Report-Only", "default-src 'self' 'nonce" + nonceVal + "" + "; report-uri /csp-report")
+        res.send(Mustache.render(content, {nonce: nonceVal}))
     })
   
+})
+
+app.post('/csp-report', (req, res) => {
+    res.send('')
 })
 
 app.use(express.static('public'))
