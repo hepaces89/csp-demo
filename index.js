fff551c35399f01cec35e203e2d6fcce15dceaaa
Hubert Pan <hubert.pan3@gmail.com>
2021-11-21 15:43:06 -0500

    add vue lib to ui

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
         // should be a dynamically generated value but using a constant for demo purposes
         const nonceVal = '2726c7f26c'
         // e.g. for production purposes, use something like: crypto.randomBytes(20).toString('hex');
-        const csp = req.query.cspPolicy?req.query.cspPolicy: "default-src 'self' 'nonce-2726c7f26c'; report-uri /csp-report; img-src 'self' *.imgur.com; script-src 'self' code.jquery.com"
+        const csp = req.query.cspPolicy?req.query.cspPolicy: "default-src 'self'; report-uri /csp-report; img-src 'self' *.imgur.com; script-src 'self' code.jquery.com 'nonce-2726c7f26c' cdn.jsdelivr.net"
         res.set("Content-Security-Policy", csp)
         res.send(Mustache.render(content, {nonce: nonceVal, policy: csp}))
     })
