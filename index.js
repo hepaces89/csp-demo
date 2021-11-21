ed0eab5bbec2d76d63c6fd2ca28d687320b412f3
Hubert Pan <hubert.pan3@gmail.com>
2021-11-21 13:21:48 -0500

    add hardcoded nonce

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const port = 3000
 app.get('/', (req, res) => {
     fs.readFile('./templates/template.html', 'utf8', (err, content) => {
         // should be a dynamically generated value but using a constant for demo purposes
-        const nonceVal = 'aRrnd0mN0nc3'
+        const nonceVal = '2726c7f26c'
         // e.g. for production purposes, use something like: crypto.randomBytes(20).toString('hex');
-        const csp = req.query.cspPolicy?req.query.cspPolicy: "default-src 'self' 'nonce-" + nonceVal + "'" + "; report-uri /csp-report"
+        const csp = req.query.cspPolicy?req.query.cspPolicy: "default-src 'self' 'nonce-2726c7f26c'; report-uri /csp-report; img-src 'self' *.imgur.com; script-src 'self' code.jquery.com"
         res.set("Content-Security-Policy", csp)
         res.send(Mustache.render(content, {nonce: nonceVal, policy: csp}))
     })
