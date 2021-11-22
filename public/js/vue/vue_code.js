3cd3d4ed0dddf253ff0d925a4bbe6f7939ac1699
Hubert Pan <hubert.pan3@gmail.com>
2021-11-21 21:27:28 -0500

    add vue code

---
+++ public/js/vue/vue_code.js
@@ -0,0 +1,7 @@
+// Raw view code (will fail without unsafe-eval (needs pre-render to avoid unsafe eval))
+var app = new Vue({
+    el: '#vue-app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
