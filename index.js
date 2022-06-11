dbe19f2bf6651e810db5633a3fc0701f2c246802
Hubert Pan <hubert.pan3@gmail.com>
2022-06-11 10:23:23 -0400

    checkpoint

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ const port = 3000
 
 app.get('/', (req, res) => {
     fs.readFile('./templates/template.html', 'utf8', (err, content) => {
-        // should be a dynamically generated value but using a constant for demo purposes
+        // should be a dynamically generated value but using a cookie value for demo purposes
         const nonceVal = '2726c7f26c'
         // e.g. for production purposes, use something like: crypto.randomBytes(20).toString('hex');
-        const csp = req.query.cspPolicy?req.query.cspPolicy: "default-src 'self'; report-uri /csp-report; img-src 'self' *.imgur.com; script-src 'self' code.jquery.com 'nonce-2726c7f26c' cdn.jsdelivr.net"
+        const csp = req.query.cspPolicy?req.query.cspPolicy: "default-src 'self'; report-uri /csp-report; img-src 'self' *.imgur.com; script-src 'self' code.jquery.com 'nonce-" + nonceVal + "' cdn.jsdelivr.net"
         res.set("Content-Security-Policy", csp)
+        res.set("Content-Type", "text/html; charset=UTF-8")
+        res.cookie("csp_guide_cookie", nonceVal)
         res.send(Mustache.render(content, {nonce: nonceVal, policy: csp}, {}, [ '<%', '%>' ]))
     })
   
+})
+
+app.get('/a', (req, res)=>{
+    console.log("recieved call to /a")
+    res.send("hi");
 })
 
 app.post('/csp-report', (req, res) => {
---
+++ public/js/vue/vue_compiled.js
@@ -0,0 +1,5 @@
+new Vue({
+    components: {
+      demo: myLib
+    }
+  }).$mount('#app')
